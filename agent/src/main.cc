ee83cba70092c5ffc9bacec3a8a6dd3df456c669
kadds <itmyxyf@gmail.com>
2020-09-17 23:21:50 +0800

    log trasfer

--- agent/src/main.cc
+++ agent/src/main.cc
@@ -1,3 +1,9 @@
+#include "asio.hpp"
+#include "bsoncxx/builder/stream/document.hpp"
+#include "bsoncxx/json.hpp"
+#include "cpptoml.h"
+#include "mongocxx/client.hpp"
+#include "mongocxx/instance.hpp"
 #include <ext/stdio_filebuf.h>
 #include <fstream>
 #include <iostream>
@@ -8,21 +14,21 @@
 #include <unistd.h>
 #include <vector>
 
-#include "bsoncxx/builder/stream/document.hpp"
-#include "bsoncxx/json.hpp"
-#include "cpptoml.h"
-#include "mongocxx/client.hpp"
-#include "mongocxx/instance.hpp"
-
 using namespace std;
+
 string gUri;
 string gDBName;
 string gMonitorCol;
 string gServiceCol;
+string gBindAddress;
+uint16_t gPort;
+uint64_t gMaxLogSize;
+string gLogCol;
 
 mongocxx::uri gen_uri() { return {gUri}; }
 mongocxx::v_noabi::collection get_monitor_col(mongocxx::client &cli) { return move(cli[gDBName][gMonitorCol]); }
 mongocxx::v_noabi::collection get_service_col(mongocxx::client &cli) { return move(cli[gDBName][gServiceCol]); }
+mongocxx::v_noabi::collection get_logger_col(mongocxx::client &cli) { return move(cli[gDBName][gLogCol]); }
 
 bool startWith(const string &str, const char *target, const char **out = nullptr)
 {
@@ -196,7 +202,8 @@ void do_docker_monitor()
         while (getline(is, s))
         {
             cout << s << endl;
-            if (s == "exec fail" || s.find("permission denied") != std::string::npos)
+            if (s == "exec fail" || s.find("permission denied") != std::string::npos ||
+                s.find("Cannot connect to") != std::string::npos)
             {
                 cout << "Exec docker command fail. Checking permissions\n";
                 return;
@@ -255,14 +262,16 @@ void do_docker_monitor()
         }
         mongocxx::client conn{gen_uri()};
         auto col = get_service_col(conn);
+        vector<bsoncxx::document::value> docs;
         for (auto &info : vec)
         {
             document doc;
             auto val = doc << "name" << info.name << "data" << open_array << (float)info.cpu << (float)info.memcost
                            << (float)info.net_in << (float)info.net_out << (float)info.block_in << (float)info.block_out
                            << close_array << finalize;
-            col.insert_one(val.view());
+            docs.emplace_back(move(val.view()));
         }
+        col.insert_many(docs);
     }
 }
 
@@ -313,7 +322,205 @@ void read_config(int argc, char *argv[])
         cout << "mongodb.serviceColumnName is empty" << endl;
         exit(-1);
     }
+    auto logCol = config->get_qualified_as<string>("mongodb.logColumnName");
+    if (!logCol)
+    {
+        cout << "mongodb.logColumnName is empty" << endl;
+        exit(-1);
+    }
+    gLogCol = logCol.value_or("");
+
     gServiceCol = serviceCol.value_or("");
+    auto host = config->get_qualified_as<string>("log.bind");
+    if (!host)
+    {
+        cout << "log.bind is empty. set 127.0.0.1" << endl;
+        exit(-1);
+    }
+    gBindAddress = host.value_or("127.0.0.1");
+    if (gBindAddress == "localhost")
+    {
+        gBindAddress = "127.0.0.1";
+    }
+    auto port = config->get_qualified_as<uint16_t>("log.port");
+    if (!port)
+    {
+        cout << "log.port is empty. set 10900" << endl;
+    }
+    gPort = port.value_or(10900);
+    auto maxSize = config->get_qualified_as<uint64_t>("log.maxSizePer");
+    if (!maxSize)
+    {
+        cout << "log.maxSizePer is empty. set 4096" << endl;
+    }
+    gMaxLogSize = maxSize.value_or(4096);
+}
+
+// single log
+// vid timestamp trackId serverName level a detail
+void send_log(std::shared_ptr<char[]> data, vector<pair<size_t, size_t>> ranges)
+{
+    cout << "post ranges " << ranges.size() << endl;
+    mongocxx::client conn{gen_uri()};
+    auto col = get_logger_col(conn);
+    vector<bsoncxx::document::value> docs;
+    for (auto &range : ranges)
+    {
+        char *start = data.get() + range.first;
+        char *end = data.get() + range.second;
+
+        auto space = std::find(start, end, ' ');
+        auto vid = strtoull(start, 0, 10);
+
+        start = space + 1;
+        space = std::find(start, end, ' ');
+        uint64_t tm = strtoull(start, 0, 10);
+
+        start = space + 1;
+        if (start >= end)
+            continue;
+        space = std::find(start, end, ' ');
+        string tid(start, space - start);
+
+        start = space + 1;
+        if (start >= end)
+            continue;
+        space = std::find(start, end, ' ');
+        string serverName(start, space - start);
+
+        start = space + 1;
+        if (start >= end)
+            continue;
+        space = std::find(start, end, ' ');
+        string level(start, space - start);
+
+        start = space + 1;
+        if (start >= end)
+            continue;
+        string_view detail(start, end - start);
+        string detail_str(start, end - start);
+
+        document doc;
+        auto val = doc << "vid" << (double)vid << "timestamp" << (double)tm << "tid" << tid << "server" << serverName
+                       << "level" << level << "detail" << detail_str << finalize;
+
+        // cout << "vid " << vid << " tm " << tm << " tid " << tid << " server " << serverName << " level " << level
+        //      << " detail " << detail_str << endl;
+        docs.emplace_back(move(val.view()));
+    }
+    if (docs.size() > 0)
+        col.insert_many(docs);
+}
+
+struct client_data
+{
+    shared_ptr<char[]> buffer;
+    std::size_t last_pos;
+    std::size_t data_pos;
+    bool discard;
+    asio::ip::tcp::socket peer;
+    client_data(asio::ip::tcp::socket peer)
+        : last_pos(0)
+        , discard(false)
+        , data_pos(0)
+        , peer(move(peer))
+    {
+        reset();
+    }
+    void reset()
+    {
+        buffer = shared_ptr<char[]>(new char[gMaxLogSize]);
+        last_pos = 0;
+        discard = false;
+    }
+};
+
+void read_done(std::shared_ptr<client_data> client, asio::error_code error, std::size_t bytes_recv,
+               asio::thread_pool &pool)
+{
+    vector<pair<size_t, size_t>> ranges;
+    shared_ptr<char[]> buffer;
+    char *buf_beg = client->buffer.get() + client->last_pos;
+    char *buf_end = client->buffer.get() + client->last_pos + bytes_recv;
+    while (buf_beg < buf_end)
+    {
+        auto cur_pos = std::find(buf_beg, buf_end, '\n');
+        size_t cur_size = cur_pos - buf_beg;
+        if (cur_pos == buf_end) // not end \n
+        {
+            if (cur_pos == client->buffer.get() + gMaxLogSize) // not space
+            {
+                // cout << "new buf\n";
+                buffer = shared_ptr<char[]>(new char[gMaxLogSize]);
+                memcpy(buffer.get(), buf_beg, cur_size);
+                client->last_pos = cur_size;
+                client->data_pos = 0;
+            }
+            else
+            {
+                // cout << "not end \n";
+                client->last_pos += cur_size;
+                client->data_pos = buf_beg - client->buffer.get();
+            }
+            break;
+        }
+        else
+        {
+            // cout << "client last " << client->data_pos << "size " << (cur_size + client->last_pos - client->data_pos)
+            //      << endl;
+            if (client->discard)
+            {
+                client->discard = false;
+            }
+            else
+            {
+                ranges.emplace_back(client->data_pos, client->last_pos + cur_size);
+            }
+            // plus 1 skip \n
+            buf_beg += cur_size + 1;
+            client->last_pos += cur_size + 1;
+            client->data_pos = client->last_pos;
+        }
+    }
+    if (ranges.size() > 0)
+    {
+        asio::post(pool, std::bind(&send_log, client->buffer, move(ranges)));
+    }
+    if (buffer)
+    {
+        client->buffer = buffer;
+    }
+    client->peer.async_read_some(
+        asio::mutable_buffer(client->buffer.get() + client->last_pos, gMaxLogSize - client->last_pos),
+        std::bind(&read_done, client, std::placeholders::_1, std::placeholders::_2, ref(pool)));
+}
+
+void accept_asio(asio::ip::tcp::acceptor &acc, asio::thread_pool &pool)
+{
+    acc.async_accept([&pool, &acc](asio::error_code error, asio::ip::tcp::socket peer) {
+        accept_asio(acc, pool); // do next
+        cout << "new client" << endl;
+        bool discard = false;
+        if (error)
+        {
+            return;
+        }
+
+        auto client = make_shared<client_data>(move(peer));
+        client->peer.async_read_some(
+            asio::mutable_buffer(client->buffer.get(), gMaxLogSize),
+            std::bind(&read_done, client, std::placeholders::_1, std::placeholders::_2, ref(pool)));
+    });
+}
+
+void log_loop()
+{
+    asio::thread_pool pool;
+    asio::io_context context(1);
+    asio::ip::address bind_address = asio::ip::address::from_string(gBindAddress);
+    asio::ip::tcp::acceptor acceptor(context, asio::ip::tcp::endpoint(bind_address, gPort), true);
+    accept_asio(acceptor, pool);
+    context.run();
 }
 
 void main_loop()
@@ -341,6 +548,7 @@ void main_loop()
 int main(int argc, char *argv[])
 {
     read_config(argc, argv);
+    std::thread thd(log_loop);
     main_loop();
     return 0;
 }
